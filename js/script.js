0e3d3f05cc1f921484ea5420266943bde04a3606
EC2 Default User <ec2-user@ip-10-0-1-208.ec2.internal>
2024-08-23 09:05:12 +0000

    Initial commit - Progetto AWS e applicazione web scalabile

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = 'Questo messaggio è stato aggiunto dinamicamente con JavaScript!';
+});
